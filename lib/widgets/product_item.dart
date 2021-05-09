cd1c1b7a52be9dc9cb433c19ef36b5966341fa69
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2021-05-09 07:11:39 +0700

    Bahan Ajar

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
 
-class ProductItem extends StatefulWidget {
+class ProductItem extends StatelessWidget {
   ProductItem(this.prodId, this.title, this.date);
 
   final String prodId, title;
@@ -12,19 +12,14 @@ class ProductItem extends StatefulWidget {
   final DateTime date;
 
   @override
-  _ProductItemState createState() => _ProductItemState();
-}
-
-class _ProductItemState extends State<ProductItem> {
-  @override
   Widget build(BuildContext context) {
     var products = Provider.of<Products>(context, listen: false);
     return ListTile(
       leading: CircleAvatar(),
-      title: Text("${widget.title}"),
-      subtitle: Text(DateFormat.yMMMEd().format(widget.date)),
+      title: Text("$title"),
+      subtitle: Text(DateFormat.yMMMEd().format(date)),
       trailing: IconButton(
-        onPressed: () => products.deleteProduct(widget.prodId),
+        onPressed: () => products.deleteProduct(prodId),
         icon: Icon(
           Icons.delete,
           color: Colors.red,
